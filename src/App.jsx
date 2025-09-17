e74e9e8777275653e0e142a9423d3b87b2a7d764
morello <morellcrein@gmail.com>
2025-09-17 22:02:14 +0100

    Add main App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import ValueProps from './components/ValueProps';
+import Features from './components/Features';
+import Security from './components/Security';
+import HowItWorks from './components/HowItWorks';
+import Pricing from './components/Pricing';
+import AboutUs from './components/AboutUs';
+import BlogPreview from './components/BlogPreview';
+import ContactCTA from './components/ContactCTA';
+import Footer from './components/Footer';
 
-const App = () => {
+function App() {
     return (
-        <div>
-            <h1>MoneySafe Vault</h1>
-        </div>
+        <>
+            <Navbar />
+            <main>
+                <HeroSection />
+                <ValueProps />
+                <Features />
+                <Security />
+                <HowItWorks />
+                <Pricing />
+                <AboutUs />
+                <BlogPreview />
+                <ContactCTA />
+            </main>
+            <Footer />
+        </>
     );
-};
+}
 
 export default App;
